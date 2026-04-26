d1d07e03c3a18d7f406f331c83ca38aa3311e4db
Lucas <lucasplaygaemes@gmail.com>
2026-04-26 11:59:59 -0300

    SO update with a lot of new things, will be diclosed in the README file

--- so.c
+++ so.c
@@ -1,10 +1,9 @@
-/*     THIS PROGRM IS MADE ONLY FOR EDUCATIONAL PURPOUSES     */
-/*     The objctive of this code is to make a program where   */
-/*     Will change the priority of the program QEMU and it's  */
-/*     Process to take the best possible of the system on use */
-/*     It also will restore, it's made with a specific        */
-/*     processor architecture in mind, may work on others.    */
-
+/*     THIS PROGRM IS MADE ONLY FOR EDUCATIONAL PURPOUSES 
+       The objctive of this code is to make a program where
+       Will change the priority of the program QEMU and it's 
+       Process to take the best possible of the system on use 
+       It also will restore, it's made with a specific
+       processor architecture in mind, may work on others.      */
 
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -16,8 +15,13 @@
 #include <string.h>
 #include <termios.h>
 #include <poll.h>
+#include <sys/sysinfo.h>
+
+#define CONFIG_FILE "so.conf"
 
 int global_qemu_pid = 0;
+
+struct termios old_t;
 
 // Funtion change the terminal mode
 void set_conio_terminal_mode() {
@@ -63,101 +67,158 @@ void restore_system(int sig) {
     write_sys_file("/proc/sys/vm/nr_hugepages", "0");
     printf("RAM restored.\n");
 
-    
     // Restore the IRQ Affinity, Mask 'fff' allows the 12 threads
     // Allows the kernel to use any core for interruptions again.
     write_sys_file("/proc/irq/default_smp_affinity", "fff");
     
-     // Retuns QEMU to the default priority
+    // Restore the Compaction proactiveness to normal
+    write_sys_file("/proc/sys/compaction_proactiveness", "20");
+    
+    // Retuns QEMU to the default priority
     if (global_qemu_pid > 0) {
         struct sched_param param;
         sched_setscheduler(global_qemu_pid, SCHED_OTHER, &param);
         printf("QEMU (PID %d) returned to the default escalator.\n", global_qemu_pid);
     }
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_t);
+    
     printf("System clean, Exiting...\n");
     exit(0);
 }
 
-int main() {
-    printf("--- PREPARING THE AMBIENT FOR THE MAX PERFORMACE ---\n");
 
-    // Try to find the VM 
-    global_qemu_pid = get_qemu_pid();
+void auto_discover(float *ram_p, int *hugepages) {
+    struct sysinfo si;
+    sysinfo(&si);
     
-    if (global_qemu_pid <= 0) {
-        fprintf(stderr, "Error! QEMU process not find, verify if the VM is active.\n");
-        return -1;
+    // Find the total RAM in the system
+    long total_ram_mb = (si.totalram * si.mem_unit) / (1024 * 1024);
+    // Find the number of cores in the system
+    int cores = sysconf(_SC_NPROCESSORS_ONLN);
+    
+    printf("\n --- FASE OF DISCOVERY ---\n");
+    printf("Hardware: %ld MB RAM | %d Threads detected.\n", total_ram_mb, cores);
+    printf("Which porcantage of ram will be dedicated to the VM (Ex.: 0.5 for 50%%)? ");
+    scanf("%f", ram_p);
+    
+    *hugepages = (int)(total_ram_mb * (*ram_p) / 2);
+    
+    FILE *f = fopen(CONFIG_FILE, "w");
+    if (f) {
+        fprintf(f, "RAM_PERCENT=%.2f\nTOTAL_HUGEPAGES=%d\n", *ram_p, *hugepages);
+        fclose(f);
+        printf("Configurations saved in %s\n", CONFIG_FILE);
     }
-    
-    printf("VM Detected with the PID: %d\n", global_qemu_pid);
-    
-    // Handles the "CTRL+C" signal, When closed it will restore to default automatically
-    signal(SIGINT, restore_system);
-    
-    // Changing the kernel aggressiveness
-    write_sys_file("/proc/sys/vm/compaction_proactiveness", "100");
-    
-    printf("Cleaning the cache and soliciting the 16GB of Hugepages...\n");
-    write_sys_file("/proc/sys/vm/drop_caches", "3");
-    
-    write_sys_file("/proc/sys/vm/compact_memory", "1");
+}
 
-    sleep(2);
-    
-    write_sys_file("/proc/sys/vm/nr_hugepages", "6144");
-    
-    FILE *f = fopen("/proc/sys/vm/nr_hugepages", "r");
-    int allocated = 0;
-    fscanf(f, "%d", &allocated);
+int load_config(float *ram_p, int *hugepages) {
+    FILE *f = fopen(CONFIG_FILE, "r");
+    if (!f) return 0;
+    if (fscanf(f, "RAM_PERCENT=%f\nTOTAL_HUGEPAGES=%d\n", ram_p, hugepages) != 2) {
+        fclose(f);
+        return 0;
+    }
     fclose(f);
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    float ram_percent = 0;
+    int target_hugepages = 0;
+    int force_clean = (argc > 1 && strcmp(argv[1], "-c") == 0);
     
-    if (allocated < 6144) {
-        fprintf(stderr, "Allocation incomplete: Only got %d pages.\n", allocated);
-        restore_system(0);
-        return -1;
+    if (!load_config(&ram_percent, &target_hugepages)) {
+        auto_discover(&ram_percent, &target_hugepages);
     }
     
-    // Apply the Real-Time priority
-    struct sched_param param;
-    param.sched_priority = 99;
+    signal(SIGINT, restore_system);
+    printf("--- PREPARING THE AMBIENT FOR SO ---\n");
+    // Variables to keep track of the memory and number of trys to allocate it
+    int allocated = 0;
+    int attempts = 0;
+    char hp_val[16];
+    snprintf(hp_val, 16, "%d", target_hugepages);
     
-    if (sched_setscheduler(global_qemu_pid, SCHED_FIFO, &param) == -1) {
-        fprintf(stderr, "ERROR! Run with sudo! (Permission Denied for SHCED_FIFO)\n");
-        restore_system(0);
-        return -1;
+    while (allocated < target_hugepages && attempts < 3) {
+        attempts++;
+        printf("\r[Attempt %d] Compacting the RAM and requesting the Pages...", attempts);
+        
+        fflush(stdout);
+        
+        printf("Cleaning the cache and soliciting the %d MB of Hugepages...\n", allocated * 2);
+        write_sys_file("/proc/sys/vm/drop_caches", "3");
+        
+        // Changhing the kernel's compact memory state
+        write_sys_file("/proc/sys/vm/compact_memory", "1");
+        
+        // Setting the hugepages value  
+        write_sys_file("/proc/sys/vm/nr_hugepages", hp_val);
+        
+        // Changing the kernel aggressiveness
+        write_sys_file("/proc/sys/vm/compaction_proactiveness", "100");
+
+        sleep(2);
+        
+        FILE *f = fopen("/proc/sys/vm/nr_hugepages", "r");
+        if (f) {
+            fscanf(f, "%d", &allocated);
+            fclose(f);
+        }
+    }    
+    
+    // Verify if could allocate    
+    if (allocated < target_hugepages) {
+      fprintf(stderr, "[X] ERROR: Allocation incomplete: Only got %d pages.\n", allocated);
+      restore_system(0);
+      return -1;
     }
-    
-    // Isolate all the IRQs and E-cores (CPUs 4-11 -> mask f0)
-    write_sys_file("/proc/irq/default_smp_affinity", "f0");
-    
-    
+    printf("\n[✔] Success: %d MB reserved, now start the VM!\n", allocated * 2);
+
     set_conio_terminal_mode();
-    
-    printf("Optimizations sucessfully applied!\n");
-    printf("Press 'q' or 'z' to exit and restore to default.\n");
-    
+      
     struct pollfd fds[1];
     fds[0].fd = STDIN_FILENO;
     fds[0].events = POLLIN;
-    
-    while (1) {
-        // Verify if any key is pressed (timeout of 500ms)
-        if (poll(fds, 1, 500) > 0) {
-            char c;
-            read(STDIN_FILENO, &c, 1);
-            if (c == 'q' || c =='Q' || c == 'z' || c == 'Z') {
-                restore_system(0);
-            }
-        }
-        
-        // Verify if the QEMU is still active
-        if (get_qemu_pid() <= 0) {
-            printf("\n VM closed detected, Cleaning...\n");
-            restore_system(0);
-        }
-    }
-    
-    while (1) { pause(); }
-    
-    return 0;
+      
+    // PID detection Loop      
+    while ((global_qemu_pid = get_qemu_pid()) <= 0) {
+    // Verify if any key is pressed (timeout of 500ms)
+    if (poll(fds, 1, 100) > 0) {
+        char c;
+        read(STDIN_FILENO, &c, 1);
+        if (c == 'q' || c =='Q' || c == 'z' || c == 'Z') {
+              restore_system(0);
+          }
+          usleep(100000);
+      }
+  }
+  printf("\n[+] VM detected (PID: %d). Applying prioritys...\n", global_qemu_pid);
+  // Apply the Real-Time priority
+  struct sched_param param;
+  param.sched_priority = 99;
+      
+  if (sched_setscheduler(global_qemu_pid, SCHED_FIFO, &param) == -1) {
+      fprintf(stderr, "ERROR! Run with sudo! (Permission Denied for SHCED_FIFO)\n");
+      restore_system(0);
+      return -1;
+  }
+  
+  // Isolate all the IRQs and E-cores (CPUs 4-11 -> mask f0)
+  write_sys_file("/proc/irq/default_smp_affinity", "f0");
+  
+  printf("Optimizations sucessfully applied!\n");
+  printf("Press 'q' or 'z' to exit and restore to default.\n");
+  
+  while (1) {
+      if (poll(fds, 1, 500) > 0) {
+          char ch; read(STDIN_FILENO, &ch, 1);
+          if (ch == 'q' || ch == 'c' || ch == 'Q' || ch == 'Z') restore_system(0);
+          }
+      // Verify if the QEMU is still active
+      if (get_qemu_pid() <= 0) {
+          printf("\n[!] VM closed detected, Cleaning...\n");
+          restore_system(0);
+      }
+  }
+  return 0;
 }
